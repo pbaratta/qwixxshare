d0604000dca277c4510ac693f405c30afadbceb3
Peter Baratta <peter.baratta@gmail.com>
2020-08-05 19:02:16 -0400

    get rid of chat, call it quixx

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'channels',
-    'chat',
+    'quixx',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/quixx/views.py
+++ mysite/quixx/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'quixx/index.html')
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
+    return render(request, 'quixx/room.html', {
         'room_name': room_name
     })
